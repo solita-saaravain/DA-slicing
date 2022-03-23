252e6d6fd77297064258703111269525c6a489ed
Saara Vainikainen <saara.vainikainen@solita.fi>
2022-03-23 14:53:44 +0200

    Comments added

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+#simple test to prepare for next excerise to see if IDE is working
 answer = input("Is this working? Y = yes, N = no: ")
+
+#let's trim the given input and change it into UpperCase
 answer = answer.strip().upper()
 
+#check the input and act accordingly
 if answer == "Y":
     print("It is working! Great success!")
 if answer == "N":
